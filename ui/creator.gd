3a34190a8fd540b74d2cae4aed63e5915420abad
Matthew Rapati <mattrapati@gmail.com>
2024-12-25 14:01:15 -0500

    Setup window/res settings; Some art and stuff

--- ui/creator.gd
+++ ui/creator.gd
@@ -1,16 +1,26 @@
 extends Control
 
 var nameNode = null;
+var studyNode = null;
+var studySelected = "fire"
 
 func _ready():
-	nameNode = get_node("Control/Control/Name");
-	pass 
+	studyNode = get_node("Name/Study");
+	nameNode = get_node("Name/Input");
 
 func _on_awaken_button_pressed():
-	
 	var character_choices = {
 		"name": nameNode.text,
-		"study": "fire"
+		"study": studySelected
 	}
 
 	Events.broadcast("AWAKEN", character_choices);
+
+
+func _on_fire_pressed():
+	studySelected = "fire"
+	$Avatar.texture = load("res://textures/" + studySelected + "-player.png");
+
+func _on_water_pressed():
+	studySelected = "water"
+	$Avatar.texture = load("res://textures/" + studySelected + "-player.png");
--- ui/overworld.gd
+++ ui/overworld.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _ready():
 	$Name.text = Manager.character.name;
-	print(Manager.character);
+	$Study.texture = load("res://textures/" + Manager.character.study + ".png");
 
 func _process(delta):
 	pass
