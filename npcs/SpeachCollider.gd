2ad78834e07c80ea8e1ec466419a6e9d957bc3fe
Matthew Rapati <mattrapati@gmail.com>
2025-01-18 00:56:35 -0500

    Add support for responses to refactored speech

--- npcs/SpeachCollider.gd
+++ npcs/SpeachCollider.gd
@@ -13,9 +13,9 @@ func _process(delta):
 	var actionKey = Input.is_action_just_pressed("action");
 	
 	if entered and actionKey:
-		actionKey();
-			
-func actionKey():
+		showCurrentSpeech();
+
+func showCurrentSpeech():
 	if giving_items:
 		Manager.dismiss_items();
 	if end == true:
@@ -25,18 +25,41 @@ func actionKey():
 			$SpeechBubble.visible = false;
 	elif parent.current:
 		var speech = parent.convo[parent.current];
+
 		if "func" in speech:
 			parent.call(speech.func);
+		#elif "responses" in speech:
+			#print("MUST CHOOSE");
 		else:
 			if "items" in speech and speech.items:
 				_give_items(speech.items);
 			else:
+				if "responses" in speech:
+					speech.buttons = _gen_buttons(speech.responses);
 				Manager.speech_bubble(speech);
 
 			if "next" in speech:
 				parent.current = speech["next"];
 			if "end" in speech and speech.end == true:
 				end = true;
+
+func _gen_buttons(responses):
+	var buttons = [];
+	for i in range(responses.size()):
+		var r = responses[i];
+		var button = Button.new();
+		button.text = r.text;
+		button.pressed.connect(self._button_pressed.bind(i));
+		buttons.append(button);
+	return buttons
+
+#this code and the code in action key should share a func!
+func _button_pressed(index):
+	var speech = parent.convo[parent.current];
+	var choice = speech.responses[index].next;
+	
+	parent.current = choice;
+	showCurrentSpeech();
 
 func _give_items(items):
 	giving_items = true;
--- npcs/oracle.gd
+++ npcs/oracle.gd
@@ -30,60 +30,26 @@ var convo = {
 		"name": displayName,
 		"image": "oracle.png",
 		"text": "Go away!",
-		"func": "_end"
+		"end": true,
+		"next": "rude"
 	},
 	"apleasure": {
 		"name": displayName,
 		"image": "oracle.png",
 		"text": "A pleasure to meet you, I am the Oracle of This Place!",
-		"func": "_end"
+		"end": true,
+		"next": "sopolite"
 	},
-	"nothing": {
+	"sopolite": {
 		"name": displayName,
 		"image": "oracle.png",
-		"text": "...",
+		"text": "You're so polite!",
+		"end": true
+	},
+	"rude": {
+		"name": displayName,
+		"image": "oracle.png",
+		"text": "You're rude!",
+		"end": true
 	}
 }
-var speaking = false;
-
-func speakKey():
-	var speech = get_speech(current);
-	Manager.speech_bubble(speech);
-	speaking = true;
-
-func actionKey():
-	if speaking:
-		if "responses" in convo[current]:
-			print("MUST CHOOSE");
-		elif "next" in convo[current]:
-			var next = convo[current]["next"];
-			var speech = get_speech(next);
-			current = next;
-			Manager.speech_bubble(speech);
-		elif "func" in convo[current]:
-			self.call(convo[current].func)
-		else:
-			Manager.speech_bubble_close();
-			speaking = false;
-
-func gen_buttons(responses):
-	var buttons = [];
-	for i in range(responses.size()):
-		var r = responses[i];
-		var button = Button.new();
-		button.text = r.text;
-		button.pressed.connect(self._button_pressed.bind(i));
-		buttons.append(button);
-	return buttons
-
-func get_speech(key):
-	var speech = convo[key];
-	if "responses" in speech:
-		speech.buttons = gen_buttons(speech.responses);
-	return speech;
-
-func _button_pressed(index):
-	var next = convo[current].responses[index].next;
-	var speech = get_speech(next);
-	current = next
-	Manager.speech_bubble(speech);
