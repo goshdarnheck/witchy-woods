c2246628a4cf632c6d09142afbd360c1a3e84fe8
Matthew Rapati <mattrapati@gmail.com>
2025-03-04 21:03:42 -0500

    Start adding pickups

---
+++ interactables/pickup.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var entered = false;
+var parent = null;
+
+
+func _on_area_entered(area):
+	print("PICKUP ENTERED")
+
+
+func _on_area_exited(area):
+	pass # Replace with function body.
